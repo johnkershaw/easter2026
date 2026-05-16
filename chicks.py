3eee109ab2b95a9fc2a9a4cbabf2a2a6847136a9
John Kershaw <johnmkershaw@gmail.com>
2026-05-16 06:26:12 +0100

    Fix .setup & .begin_fill

--- chicks.py
+++ chicks.py
@@ -3,17 +3,19 @@ import turtle
 WIDTH =  800
 HEIGHT = 400
 screen = turtle.Screen()
-screen.setup = (WIDTH, HEIGHT)
+screen.setup (WIDTH, HEIGHT)
 t = turtle.Turtle()
 
 def draw_chick(x = 90, colour = 'yellow'):
  t.circle(50)
- t.begin_fill(colour)
+ t.fillcolor(colour)
+ t.begin_fill()
  t.forward(x)
  t.pendown()
  t.back(30)
  t.forward(70)
  t.seth(180)
+ t.end_fill()
 
 # body 
 t.penup()
